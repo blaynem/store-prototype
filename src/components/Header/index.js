423c37d6624313f17ecbf2517c6435ccdc641a31
Blayne Marjama <Blayne.marjama@gmail.com>
2017-02-04 17:59:55 -0800

    refactored a bit, i'm learning and it's glorious

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,27 @@ import { Link } from 'react-router';
 export default class Header extends Component {
 
   render() {
+    const titleDivStyle = {
+      margin:"0 10px",
+      display:"inline-block",
+      color:"black"
+    }
+    const titleStyle = {
+      fontSize:"1.2em"
+    }
+    const titles = [["Men's Outerwear", "Ladies Outerwear", "Men's T-Shirts", "Ladies T-Shirts"], ["mens_outerwear", "ladies_outerwear", "mens_tshirts", "ladies_tshirts"]]
+    // oh my god it worked im a genius
+    const titlesList = titles[0].map(
+      (title, i) => (
+        <Link to={`/list/${titles[1][i]}`} key={titles[1][i] + i}>
+          <div style={titleDivStyle}>
+            <h3 style={titleStyle}>{titles[0][i]}</h3>
+          </div>
+        </Link>
+      )
+    )
+
+
     return(
       <div style={{ width:"100%", background:"rgba(0,0,0,0.0"}} className="container">
       	<div style={{textAlign:"center", marginBottom:"20px"}} className="row">
@@ -18,13 +39,30 @@ export default class Header extends Component {
 	        </div>
       	</div>
       	<div style={{width:"100%", textAlign:"center", marginBottom:"20px"}} className="row">
-      		<Link to="/list/mens_outerwear"><div style={{margin:"0 10px", display:"inline-block", color:"black"}}><h3 style={{fontSize:"1.2em"}}>Men's Outerwear</h3></div></Link>
-      		<Link to="/list/ladies_outerwear"><div style={{margin:"0 10px", display:"inline-block", color:"black"}}><h3 style={{fontSize:"1.2em"}}>Ladies Outerwear</h3></div></Link>
-      		<Link to="/list/mens_tshirts"><div style={{margin:"0 10px", display:"inline-block", color:"black"}}><h3 style={{fontSize:"1.2em"}}>Men's T-Shirts</h3></div></Link>
-      		<Link to="/list/ladies_tshirts"><div style={{margin:"0 10px", display:"inline-block", color:"black"}}><h3 style={{fontSize:"1.2em"}}>Ladies T-Shirts</h3></div></Link>
-      	</div>
+          {titlesList}
+        </div>
       </div>
     );
   }
 }
 
+      		// <Link to="/list/mens_outerwear">
+            // <div style={titleDivStyle}>
+              // <h3 style={titleStyle}>Men's Outerwear</h3>
+            // </div>
+          // </Link>
+      		// <Link to="/list/ladies_outerwear">
+            // <div style={titleDivStyle}>
+              // <h3 style={titleStyle}>Ladies Outerwear</h3>
+            // </div>
+          // </Link>
+      		// <Link to="/list/mens_tshirts">
+            // <div style={titleDivStyle}>
+              // <h3 style={titleStyle}>Men's T-Shirts</h3>
+            // </div>
+          // </Link>
+      		// <Link to="/list/ladies_tshirts">
+            // <div style={titleDivStyle}>
+              // <h3 style={titleStyle}>Ladies T-Shirts</h3>
+            // </div>
+          // </Link>
